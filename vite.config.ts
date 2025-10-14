0ad3ca9473b581238136d0bed9baee29091c9c7e
Samuel_OMAGO <sammuaraujo200@gmail.com>
2025-10-14 08:57:44 -0300

    primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/PaulistaoCenter/", // 🔥 mantém o nome do repositório aqui
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -19,6 +21,7 @@ export default defineConfig({
         ]
       : []),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -26,15 +29,11 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
+
   root: path.resolve(import.meta.dirname, "client"),
+
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
   },
 });
