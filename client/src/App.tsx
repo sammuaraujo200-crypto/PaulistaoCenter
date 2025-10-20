be82b2577b1d8c4551d82580997e43a43c23ca86
Samuel_OMAGO <sammuaraujo200@gmail.com>
2025-10-20 19:09:08 -0300

    feat: adiciona rota /parcerias

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Parcerias from "./pages/parcerias";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
@@ -10,6 +11,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/parcerias" component={Parcerias} />
       <Route component={NotFound} />
     </Switch>
   );
