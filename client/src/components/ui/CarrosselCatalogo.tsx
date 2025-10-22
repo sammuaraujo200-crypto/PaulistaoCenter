421146e4e22b7c71099d8b512a24354b5480d0f1
Samuel_OMAGO <sammuaraujo200@gmail.com>
2025-10-22 00:15:16 -0300

    carrossel de produtos

--- client/src/components/ui/CarrosselCatalogo.tsx
+++ client/src/components/ui/CarrosselCatalogo.tsx
@@ -40,7 +40,7 @@ export default function CarrosselCatalogo() {
                 onError={(e) => ((e.target as HTMLImageElement).style.display = "none")}
               />
               {/* Gradiente escuro somente sobre a imagem */}
-              <div className="absolute inset-x-0 bottom-0 h-28 bg-gradient-to-t from-black/60 via-black/10 to-transparent z-10" />
+              <div className="absolute inset-x-0 bottom-0 h-28 bg-whitez-10 " />
             </div>
           </SwiperSlide>
         ))}
